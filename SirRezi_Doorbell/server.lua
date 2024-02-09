a31e6b750bae8c333de4d1b32c91a9764192a261
SirRezi <138231740+SirRezi@users.noreply.github.com>
2024-02-09 23:53:40 +0100

    Update server.lua

--- SirRezi_Doorbell/server.lua
+++ SirRezi_Doorbell/server.lua
@@ -2,7 +2,7 @@ RegisterServerEvent('doorbell:ringDoorbell')
 AddEventHandler('doorbell:ringDoorbell', function()
     local coords = Config.DoorbellCoords
     if coords then
-        TriggerClientEvent('doorbell:playSound', -1) -- Alle Clients sollen den Sound abspielen
+        TriggerClientEvent('doorbell:playSound', -1)
         TriggerClientEvent('doorbell:notifyPolice', -1, coords)
     end
 end)
